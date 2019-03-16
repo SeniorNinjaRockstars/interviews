e9be11e6b779953bbd39e9795d6dd259f799deef
Rafał Rudol <rrrrudol@gmail.com>
2019-03-16 10:45:47 +0100

    #7 add draft of Landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,69 +1,22 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import { Form, Field } from "react-final-form"
-import ReCAPTCHA from "react-google-recaptcha"
-
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import Tabs from "../components/Tabs"
+import Main from "../components/Main"
+import Badge from "../components/Badge/Badge"
+import Card from "../components/Card/Card"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-
-    <Form
-      onSubmit={() => fetch("/.netlify/handle")}
-      render={({ handleSubmit, pristine, invalid }) => (
-        <form onSubmit={handleSubmit}>
-          <h2>Submit you question</h2>
-          <div>
-            <label>First Name</label>
-            <Field
-              name="firstname"
-              component="input"
-              placeholder="First Name"
-            />
-          </div>
-
-          <div>
-            <label>E-mail address</label>
-            <Field name="email" component="input" placeholder="E-mail" />
-          </div>
-
-          <div>
-            <Field
-              name="captcha"
-              render={({input}) =><ReCAPTCHA sitekey={process.env.CAPTCHA_PUBLIC} {...input} />
-              }
-            />
-          </div>
-
-          <Field
-            name="question"
-            render={({ input, meta }) => (
-              <div>
-                <label>Your question</label>
-                <textarea {...input} />
-                {meta.touched && meta.error && <span>{meta.error}</span>}
-              </div>
-            )}
-          />
-
-          <button type="submit" disabled={pristine || invalid}>
-            Submit
-          </button>
-        </form>
-      )}
-    />
-
-    <Link to="/page-2/">Go to page 2</Link>
+    <main>
+      <h1>How well do you know your C++?</h1>
+      <p>Inteit lets you browse thousends of technical IT questions</p>
+      <Link to="browse">Browse questions</Link>
+    </main>
   </Layout>
 )
 
