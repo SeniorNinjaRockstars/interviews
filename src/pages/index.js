34a9b4ccdb557aecbfe6ea4317a862ce7808cdeb
Tomasz Gil <tomaszgil@hotmail.com>
2019-03-19 15:42:15 +0100

    Export hero config to an external file

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,29 +6,14 @@ import HeroCarousel from "../components/HeroCarousel"
 import Layout from "../containers/Layout"
 import SEO from "../containers/SEO"
 
-import ImageJS from "../images/js.svg"
-import ImagePython from "../images/python.svg"
-import ImageCPlusPlus from "../images/cplusplus.svg"
+import heroConfig from "../config/hero"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <main>
       <HeroCarousel
-        dynamicList={[
-          {
-            title: "How well do you know your JavaScript?",
-            image: ImageJS
-          },
-          {
-            title: "How well do you know your Python?",
-            image: ImagePython
-          },
-          {
-            title: "How well do you know your C++?",
-            image: ImageCPlusPlus
-          }
-        ]}
+        dynamicList={heroConfig}
         duration={8000}
       >
         <p>Interit lets you browse thousands of technical IT questions knowledge of will help you land the job you've always been dreaming of.</p>
