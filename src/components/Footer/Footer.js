9b98630162375530e150634adfc5bc269bc7ae35
Tomasz Gil <tomaszgil.dev@gmail.com>
2019-05-09 16:32:00 +0200

    Remove navigation from footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,7 +2,6 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Logo from "../../core/Logo"
-import NavList from "../NavList"
 import { Container } from "../../core/Containers";
 
 import { FooterWrapper, FooterContent } from './Footer.styled';
@@ -14,7 +13,6 @@ const Footer = () => (
         <Link to="/">
           <Logo />
         </Link>
-        <NavList />
       </FooterContent>
     </Container>
   </FooterWrapper>
